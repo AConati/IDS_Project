33b8f5dbf5cd8adec5162ccc79a03be01c6a0a9b
Ari Conati <conatia@carleton.edu>
2021-10-21 16:12:37 -0500

    LDA does something

--- cluster.py
+++ cluster.py
@@ -34,7 +34,6 @@ Note that this function is slow since it needs to consider
 several combinations of hyperparameters.
 """
 def choose_hyperparameters(X):
-    # X_train, X_val, y_train, y_val = train_test_split(X, test_size=0.2)
     search_params = {'n_components': [5, 10, 15, 20], 'learning_decay': [.5, .7, .9]}
 
     lda = LatentDirichletAllocation(max_iter=5, learning_method='online')
@@ -47,18 +46,36 @@ def choose_hyperparameters(X):
 """
 Run LDA with the given documents.
 """
-def find_topics(model, documents):
-    pass
+def find_topics(model, X):
+    topics = model.transform(X)
+    topic_names = ["Topic" + str(i) for i in range(model.n_components)]
+    df = pd.DataFrame(topics, columns=topic_names)
+
+    df["mostRelatedTopic"] = np.argmax(df.values, axis=1)
+
+    return df
 
 def main():
     df = eda.import_data("prepared_data.csv")
     df, documents = convert_loaded_text(df)
     X = get_doc_word_matrix(documents)
+    # X_train, X_test = train_test_split(X, test_size=0.2)
 
-    grid_model = choose_hyperparameters(X)
-    print(grid_model.best_params_)
-    print(grid_model.best_score_)
-    lda_model = grid_model.best_estimator_
+    """
+    Tuning hyperparameters is slow, so commented out
+    after running once. Should do again if data changes.
+    """
+
+    # print("Tuning hyperparameters...")
+    # grid_model = choose_hyperparameters(X)
+    # lda_model = grid_model.best_estimator_
+
+    lda_model = LatentDirichletAllocation(max_iter=5, learning_method='online', n_components=5,
+            learning_decay=.9)
+    lda_model.fit(X)
+
+    topic_df = find_topics(lda_model, X)
+    print(topic_df.head())
 
 if __name__ == "__main__":
     main()
